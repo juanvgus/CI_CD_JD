6787cf5cce83f54eb6f3d956abe84f839fd6a56a
juanvgus <79470139+juanvgus@users.noreply.github.com>
2023-08-24 10:16:47 -0500

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,11 @@
+def es_primo(num):
+    for n in range(2, num):
+        if num % n == 0:
+            print("No es primo", n, "es divisor")
+            return False
+    print("Es primo")
+    return True
+
+
+def test_answer():
+    assert es_primo(3) == "Es primo"
